eba6b7a2b33fd020011294c0acda8fe9a418277f
AndCook <acmagee@gmail.com>
2021-10-18 23:12:43 -0400

    Add docstring

--- nicknamer.py
+++ nicknamer.py
@@ -17,7 +17,18 @@ nicknamer = Bot(command_prefix="!")
 
 
 @nicknamer.command(name="nick")
-async def nick(context: Context, member: Member, new_nickname: str):
+async def nick(context: Context, member: Member, new_nickname: str) -> None:
+    """Routine responsible for 'nick' discord command.
+
+    This function handles the 'nick' command for the `nicknamer` bot. Its purpose is to allow discord users to manage
+    each other's nicknames, even if they are in the same Discord Role. The bot applies any nickname changes as specified
+    by this command. This command assumes that the bot has a higher Role than all users which invoke this command.
+
+    Args:
+        context: The discord `Context` from which the command was invoked
+        member: Member whose nickname should be changed
+        new_nickname: New nickname which should be applied to `member`
+    """
     original_nickname = member.nick
 
     await member.edit(nick=new_nickname)
