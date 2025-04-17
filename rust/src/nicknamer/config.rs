4461dbab121452e528baa43518f861e61f1aa3fa
tacascer <trandangtrithanh2000@gmail.com>
2025-04-16 21:09:27 -0400

    chore(config): replace unused variables with dead code allow
    Switched `#[allow(unused_variables)]` to `#[allow(dead_code)]` for constants. This change ensures proper categorization of unused code and keeps the compiler warnings aligned with intent. No functional behavior is altered.

--- rust/src/nicknamer/config.rs
+++ rust/src/nicknamer/config.rs
@@ -1,18 +1,18 @@
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const REVEAL_INSULT: &str = "ya dingus";
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const CODE_MONKEYS_ROLE_NAME: &str = "Code Monkeys";
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const URL_LENGTH_VIOLATION_FACTOR: u32 = 2;
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const JAR_JAR_EMOJI_ID: u64 = 1061775549065855079;
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const ZACH_USER_ID: u64 = 894692357457469471;
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const JAR_JAR_COLOR_HEX: u32 = 0xD59D7E;
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const HORRIFIED_JAR_JAR_PIC: &str =
     "https://cdn.mos.cms.futurecdn.net/RvLDChLaR37NWTEjvQm2pB-970-80.jpg.webp";
-#[allow(unused_variables)]
+#[allow(dead_code)]
 pub const HAPPY_JAR_JAR_PIC: &str =
     "https://static.wikia.nocookie.net/unanything/images/c/c7/Jar_Jar.jpg/revision/latest";
